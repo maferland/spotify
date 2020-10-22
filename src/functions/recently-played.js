02f6c773d19b9a43d0881320738b3d3200f0400e
Marc-Antoine Ferland <maferland@hey.com>
2020-10-21 22:15:59 -0400

    :bug: Add cors headers

--- src/functions/recently-played.js
+++ src/functions/recently-played.js
@@ -3,6 +3,12 @@ const {getUserById} = require('../fauna/get-user-by-id.js')
 const {refreshIfExpired} = require('../spotify/refresh-if-expired.js')
 const {getRecentlyPlayed} = require('../spotify/get-recently-played.js')
 
+const headers = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': 'Content-Type',
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+}
+
 exports.handler = async (event, context) => {
   const {path} = event
   const id = getId(path)
@@ -13,7 +19,7 @@ exports.handler = async (event, context) => {
     const {accessToken} = await refreshIfExpired(user, ts)
     const response = await getRecentlyPlayed(accessToken)
 
-    return {statusCode: 200, body: JSON.stringify(response)}
+    return {statusCode: 200, headers, body: JSON.stringify(response)}
   } catch (error) {
     return {statusCode: 500, body: JSON.stringify(error)}
   }
